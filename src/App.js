ecf8dec352eed31390161a759f0b5b771641a723
Tarra Co <tarra.co@me.com>
2020-05-25 22:14:10 +1000

    add Bootstrap

--- src/App.js
+++ src/App.js
@@ -6,24 +6,26 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import "./App.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import UserContext from "./UserContext";
 
-import SimpleBar from 'simplebar-react';
-import 'simplebar/dist/simplebar.min.css';
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
 // Why use SimpleBar: https://stackoverflow.com/questions/57862671/how-to-make-scroll-bar-overlay-content
 
 export default function App() {
-  const [user, setUser] = useState(localStorage.getItem("username"))
+  const [user, setUser] = useState(localStorage.getItem("username"));
   // useContext Hook Tutorial: https://www.youtube.com/watch?v=lhMKvyLRWo0
 
   return (
     <Router>
       <div className="App">
-        <SimpleBar style={{ height: '100vh' }}>
+        <SimpleBar style={{ height: "100vh" }}>
           {/*https://stackoverflow.com/questions/12172177/set-div-height-equal-to-screen-size/41537811#41537811 */}
           <Nav />
           <Switch>
-            <UserContext.Provider value={{user, setUser}}>
+            <UserContext.Provider value={{ user, setUser }}>
               <Route exact path="/">
                 <Home />
               </Route>
