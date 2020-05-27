4501fbb9f08c09109a036ceb905f5bc2b2ad253a
Simon Tran <sctran@student.unimelb.edu.au>
2020-05-28 03:04:56 +1000

    add custom hook useUser to api.js

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:3001";
@@ -5,13 +6,14 @@ const BASE_URL = "https://cityzen-app.herokuapp.com";
 const MAPBOX_BASE_URL = "https://api.mapbox.com";
 const API_TOKEN =
   "pk.eyJ1IjoiYW50aGdpYW5nIiwiYSI6ImNrOXdtNmJpZDBhem4zbG1rODNrYmxrZnAifQ.QyMjlGdfO2PcviXkyb_xVA";
+axios.defaults.baseURL = BASE_URL;
 
 export function getLogin(username, password) {
   const endpoint = BASE_URL + "/users/login";
 
   return axios.post(endpoint, {
-    username: username,
-    password: password,
+    username,
+    password,
   });
 }
 
@@ -20,13 +22,38 @@ export function getUser(username) {
   return axios.get(endpoint);
 }
 
+export function useUser(username) {
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(/users/ + username)
+      .then((res) => {
+        setUserData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, [username]);
+
+  return {
+    loading,
+    userData,
+    error,
+  };
+}
+
 export function addUser(username, password, emailAddress) {
   const endpoint = BASE_URL + "/users/new";
 
   return axios.post(endpoint, {
-    username: username,
-    password: password,
-    emailAddress: emailAddress,
+    username,
+    password,
+    emailAddress,
   });
 }
 
