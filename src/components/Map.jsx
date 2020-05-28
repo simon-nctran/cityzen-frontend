113acf1d9753c60bf2627288f81129622a732049
Simon Tran <sctran@student.unimelb.edu.au>
2020-05-28 22:15:38 +1000

    refactor map.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,7 +5,7 @@ import mapboxgl from "mapbox-gl";
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYW50aGdpYW5nIiwiYSI6ImNrOXdtNmJpZDBhem4zbG1rODNrYmxrZnAifQ.QyMjlGdfO2PcviXkyb_xVA";
 
-let TOKEN = 
+const TOKEN =
   "pk.eyJ1IjoiYW50aGdpYW5nIiwiYSI6ImNrOXdtNmJpZDBhem4zbG1rODNrYmxrZnAifQ.QyMjlGdfO2PcviXkyb_xVA";
 
 // MapBox API: https://docs.mapbox.com/mapbox-gl-js/api/
@@ -21,105 +21,118 @@ export default function Map(props) {
   // https://reactjs.org/docs/hooks-reference.html#useref
   // https://reactjs.org/docs/refs-and-the-dom.html
 
-
   // Run at initialisation and everytime props.journey is updated.
   // second argument of useEffect is an array which defines the variables it tracks for changes
   // it runs whenever one of these variables changes
   useEffect(() => {
-    console.log("Map Component has been mounted")
-    const { lng, lat, zoom } = lngLatZoom
+    console.log("Map Component has been mounted");
+    const { lng, lat, zoom } = lngLatZoom;
     const map = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
-      zoom: zoom
+      zoom, // short hand for zoom: zoom
     });
 
     map.on("move", () => {
       setLngLatZoom({
         lng: map.getCenter().lng,
         lat: map.getCenter().lat,
-        zoom: map.getZoom()
+        zoom: map.getZoom(),
         // removed .toFixed as that converts to String,
         // but latLngZoom uses Number as defined in the argument of useState above
       });
     });
 
-    // create a dummy route
+    // create a route
     map.on("load", () => {
-      map.addSource('route', {
-        'type': 'geojson',
-        'data': {
-        'type': 'Feature',
-        'properties': {},
-        'geometry': {
-          'type': 'LineString',
-          'coordinates': routeCoords
-          }
-        }
-        });
-        map.addLayer({
-        'id': 'route',
-        'type': 'line',
-        'source': 'route',
-        'layout': {
-        'line-join': 'round',
-        'line-cap': 'round'
+      map.addSource("route", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: routeCoords,
+          },
         },
-        'paint': {
-        'line-color': '#888',
-        'line-width': 8
-        }
-        });    })
+      });
+      map.addLayer({
+        id: "route",
+        type: "line",
+        source: "route",
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "#888",
+          "line-width": 8,
+        },
+      });
+    });
   }, [routeCoords]);
 
   useEffect(() => {
     console.log("props.journey has been updated");
 
     function getLngLat(place) {
-      return new Promise(function(resolve, reject) {
-        let getLngLatUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + place + ".json?proximity=144.9,-37.8&country=AU&access_token=" + TOKEN;
+      return new Promise((resolve, reject) => {
+        const getLngLatUrl =
+          "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+          place +
+          ".json?proximity=144.9,-37.8&country=AU&access_token=" +
+          TOKEN;
         fetch(getLngLatUrl)
-        .then(response => response.json())
-        .then(data => {
-          if (data.features[0] === undefined) { //features[0] is undefined if the place doesnt exist
-            reject("its undefined here")
-          } else {
-            resolve(data.features[0].geometry.coordinates)  //extract the coordinates
-          }
-        })
-      })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.features[0] === undefined) {
+              // features[0] is undefined if the place doesnt exist
+              reject("its undefined here");
+            } else {
+              resolve(data.features[0].geometry.coordinates); // extract the coordinates
+            }
+          });
+      });
     }
 
     function getRoute(start, end) {
-      return new Promise(function(resolve, reject) {
-        let getRouteUrl = "https://api.mapbox.com/directions/v5/mapbox/driving/" + start[0] + "," + start[1] + ";" + end[0] + "," + end[1] + "?steps=true&geometries=geojson&access_token=pk.eyJ1IjoiYW50aGdpYW5nIiwiYSI6ImNrOXdtNmJpZDBhem4zbG1rODNrYmxrZnAifQ.QyMjlGdfO2PcviXkyb_xVA"
+      return new Promise((resolve, reject) => {
+        const getRouteUrl =
+          "https://api.mapbox.com/directions/v5/mapbox/driving/" +
+          start[0] +
+          "," +
+          start[1] +
+          ";" +
+          end[0] +
+          "," +
+          end[1] +
+          "?steps=true&geometries=geojson&access_token=pk.eyJ1IjoiYW50aGdpYW5nIiwiYSI6ImNrOXdtNmJpZDBhem4zbG1rODNrYmxrZnAifQ.QyMjlGdfO2PcviXkyb_xVA";
         fetch(getRouteUrl)
-        .then(response => response.json())
-        .then(data => {
-          if (data.routes === undefined) {
-            reject("route is undefined")
-          } else {
-            resolve(data)
-          }
-        })
-      })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.routes === undefined) {
+              reject(new Error("route is undefined"));
+            } else {
+              resolve(data);
+            }
+          });
+      });
     }
 
-    // if start and end are not undefined then    
-    let start = props.journey.origin;
-    let end = props.journey.destination;
+    // if start and end are not undefined then
+    const start = props.journey.origin;
+    const end = props.journey.destination;
 
     // get the startCoord, then get the endCoord, then get the Route
-    getLngLat(start)
-    .then(startCoord => getLngLat(end)
-      .then(endCoord => getRoute(startCoord, endCoord))
-      .then(route => {
-        console.log(route.routes[0].geometry.coordinates);
-        setRouteCoords(route.routes[0].geometry.coordinates)}
-      )
-    )
-
+    getLngLat(start).then((startCoord) =>
+      getLngLat(end)
+        .then((endCoord) => getRoute(startCoord, endCoord))
+        .then((route) => {
+          console.log(route.routes[0].geometry.coordinates);
+          setRouteCoords(route.routes[0].geometry.coordinates);
+        })
+    );
   }, [props.journey]);
 
   const { lng, lat, zoom } = lngLatZoom;
@@ -130,8 +143,7 @@ export default function Map(props) {
     <>
       <>
         <div className="sidebarStyle">
-          Longitude: {lng} | Latitude: {lat} | Zoom:{" "}
-          {zoom}
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
         </div>
       </>
       <div ref={mapContainer} className="mapContainer" />
