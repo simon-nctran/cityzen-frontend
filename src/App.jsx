bcd206ca0b10714684d967c727feb85aff34fc64
Tarra Co <tarra.co@me.com>
2020-05-29 00:18:21 +1000

    comment out SimpleBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import SimpleBar from "simplebar-react";
-import "simplebar/dist/simplebar.min.css";
+// import SimpleBar from "simplebar-react";
+// import "simplebar/dist/simplebar.min.css";
 // Why use SimpleBar: https://stackoverflow.com/questions/57862671/how-to-make-scroll-bar-overlay-content
 
 import Nav from "./components/Nav";
@@ -11,6 +11,7 @@ import Profile from "./pages/Profile";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+/* https://react-bootstrap.netlify.app/getting-started/introduction */
 
 import UserContext from "./UserContext";
 
@@ -21,20 +22,20 @@ export default function App() {
   return (
     <Router>
       <div className="App">
-        <SimpleBar style={{ height: "100vh" }}>
-          {/* https://stackoverflow.com/questions/12172177/set-div-height-equal-to-screen-size/41537811#41537811 */}
-          <Nav />
-          <Switch>
-            <UserContext.Provider value={{ user, setUser }}>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/profile">
-                <Profile />
-              </Route>
-            </UserContext.Provider>
-          </Switch>
-        </SimpleBar>
+        {/* <SimpleBar style={{ height: "100vh" }}> */}
+        {/* https://stackoverflow.com/questions/12172177/set-div-height-equal-to-screen-size/41537811#41537811 */}
+        <Nav />
+        <Switch>
+          <UserContext.Provider value={{ user, setUser }}>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+          </UserContext.Provider>
+        </Switch>
+        {/* </SimpleBar> */}
       </div>
     </Router>
   );
