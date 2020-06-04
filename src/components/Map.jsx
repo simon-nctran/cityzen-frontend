3042743ffe51ced2328f6d85bb203556ed1bcd0f
Anthony Giang <anth@sent.com>
2020-06-05 01:29:45 +1000

    update POI search to route start

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -54,13 +54,182 @@ export default function Map(props) {
     setMap(newMap);
   }, []);
 
+  // run when the user journey input is updated
+  useEffect(() => {
+    if (map && props.journey !== {}) {
+      console.log("props.journey has been updated");
+
+      // extract the user input into their respective fields
+      const start = props.journey.origin;
+      const end = props.journey.destination;
+      // const mode = props.journey.mode;
+
+      // finds the relevant data on a waypoint
+      function searchWaypoint(place) {
+        return new Promise((resolve, reject) => {
+          const searchWaypointUrl =
+            "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+            place +
+            ".json?proximity=" +
+            lngLatZoom.lng +
+            "," +
+            lngLatZoom.lat +
+            "&country=AU&access_token=" +
+            TOKEN;
+          fetch(searchWaypointUrl)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.features[0] === undefined) {
+                // features[0] is undefined if the place doesnt exist
+                alert("Places could not be found");
+                reject("searchWaypoint reject");
+              } else {
+                console.log("searchWaypoint data:", data);
+                resolve(data); // extract the coordinates
+              }
+            });
+        });
+      }
+
+      // gets the route from a start and end latitude and longitude
+      function getRoute(start, end) {
+        return new Promise((resolve, reject) => {
+          const getRouteUrl =
+            "https://api.mapbox.com/directions/v5/mapbox/driving/" +
+            start[0] +
+            "," +
+            start[1] +
+            ";" +
+            end[0] +
+            "," +
+            end[1] +
+            "?steps=true&geometries=geojson&access_token=" +
+            TOKEN;
+          fetch(getRouteUrl)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.routes === undefined) {
+                reject(new Error("getRoute reject"));
+              } else {
+                resolve(data);
+              }
+            });
+        });
+      }
+
+      // get the startCoord, then get the endCoord, then get the Route
+      searchWaypoint(start).then((start) =>
+        searchWaypoint(end)
+          .then((end) => {
+            return getRoute(
+              start.features[0].geometry.coordinates,
+              end.features[0].geometry.coordinates
+            );
+          })
+          .then((route) => {
+            console.log("route coordinates", route.routes[0].geometry.coordinates);
+            setRouteCoords(route.routes[0].geometry.coordinates);
+          })
+      );
+    }
+  }, [props.journey]);
+
+  // draw the route (run when route coordinates are updated)
+  useEffect(() => {
+    if (map && routeCoords) {
+
+      let routeData = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'LineString',
+          coordinates: routeCoords,
+        }
+      };
+
+      // add a "route" resource to map
+      if (map.getSource("route")) {
+        map.getSource("route").setData(routeData);
+      } else {
+        map.addSource("route", {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "LineString",
+              coordinates: routeCoords,
+            },
+          },
+        });
+        // add a layer to map that displays "route" resource
+        map.addLayer({
+          id: "route",
+          type: "line",
+          source: "route",
+          layout: {
+            "line-join": "round",
+            "line-cap": "round",
+          },
+          paint: {
+            "line-color": "#888",
+            "line-width": 8,
+          },
+        });
+      }
+    }
+  }, [routeCoords]);
+
+  // search for nearby POIs
+  useEffect(() => {
+    if (routeCoords) {
+
+      const poi = "Coffee";
+
+      // finds the relevant data on a POI
+      function searchPOI(place) {
+        return new Promise((resolve, reject) => {
+          // search for POIs near along the route
+          const searchPOIUrl =
+            "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+            place +
+            ".json?proximity=" +
+            routeCoords[0][0] +
+            "," +
+            routeCoords[0][1] +
+            "&country=AU&access_token=" +
+            TOKEN;
+          fetch(searchPOIUrl)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.features === undefined) {
+                // features is undefined if the place doesnt exist
+                alert("Places could not be found");
+                reject("searchPOI reject");
+              } else {
+                console.log("searchPOI data:", data);
+                resolve(data.features); // extract the coordinates
+              }
+            });
+        });
+      }
+
+      searchPOI(poi).then((poi) => {
+        console.log("poi features:", poi);
+        // add place names to the description to display on marker
+        setPoiFeatures(poi);
+      });
+    }
+  }, [routeCoords])
+
+  //draw the POIs (run when POIs are updated)
   useEffect(() => {
 
     if (poiFeatures != null) {
 
       let poiData = {
-          type: "FeatureCollection",
-          features: poiFeatures,
+        type: "FeatureCollection",
+        features: poiFeatures,
       }
 
 
@@ -119,132 +288,6 @@ export default function Map(props) {
     }
   }, [poiFeatures]);
 
-  useEffect(() => {
-    if (map && routeCoords) {  //run only once the map is loaded and coordinates of a route exist
-
-      let routeData = {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-          type: 'LineString',
-          coordinates: routeCoords,
-        }
-      };
-
-      // add a "route" resource to map
-      if (map.getSource("route")) {
-        map.getSource("route").setData(routeData);
-      } else {
-        map.addSource("route", {
-          type: "geojson",
-          data: {
-            type: "Feature",
-            properties: {},
-            geometry: {
-              type: "LineString",
-              coordinates: routeCoords,
-            },
-          },
-        });
-        // add a layer to map that displays "route" resource
-        map.addLayer({
-          id: "route",
-          type: "line",
-          source: "route",
-          layout: {
-            "line-join": "round",
-            "line-cap": "round",
-          },
-          paint: {
-            "line-color": "#888",
-            "line-width": 8,
-          },
-        });
-      }
-    }
-  }, [routeCoords]);
-
-  useEffect(() => {
-    if (map && props.journey !== {}) {
-      console.log("props.journey has been updated");
-
-      function searchWaypoint(place) {
-        return new Promise((resolve, reject) => {
-          const searchWaypointUrl =
-            "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-            place +
-            ".json?proximity=" +
-            lngLatZoom.lng +
-            "," +
-            lngLatZoom.lat +
-            "&country=AU&access_token=" +
-            TOKEN;
-          fetch(searchWaypointUrl)
-            .then((response) => response.json())
-            .then((data) => {
-              if (data.features[0] === undefined) {
-                // features[0] is undefined if the place doesnt exist
-                alert("Places could not be found");
-                reject("its undefined here");
-              } else {
-                console.log("searchWaypoint data:", data);
-                resolve(data); // extract the coordinates
-              }
-            });
-        });
-      }
-
-      function getRoute(start, end) {
-        return new Promise((resolve, reject) => {
-          const getRouteUrl =
-            "https://api.mapbox.com/directions/v5/mapbox/driving/" +
-            start[0] +
-            "," +
-            start[1] +
-            ";" +
-            end[0] +
-            "," +
-            end[1] +
-            "?steps=true&geometries=geojson&access_token=" +
-            TOKEN;
-          fetch(getRouteUrl)
-            .then((response) => response.json())
-            .then((data) => {
-              if (data.routes === undefined) {
-                reject(new Error("route is undefined"));
-              } else {
-                resolve(data);
-              }
-            });
-        });
-      }
-
-      // if start and end are not undefined then
-      const start = props.journey.origin;
-      const end = props.journey.destination;
-      const poi = "Coffee";
-
-      // get the startCoord, then get the endCoord, then get the Route
-      searchWaypoint(start).then((start) =>
-        searchWaypoint(end)
-          .then((end) => {
-            return getRoute(
-              start.features[0].geometry.coordinates,
-              end.features[0].geometry.coordinates
-            );
-          })
-          .then((route) => {
-            console.log("route coordinates", route.routes[0].geometry.coordinates);
-            setRouteCoords(route.routes[0].geometry.coordinates);
-          })
-      );
-      searchWaypoint(poi).then((poi) => {
-        console.log("poi features:", poi.features);
-        // add place names to the description to display on marker
-        setPoiFeatures(poi.features);
-      });
-    }
-  }, [props.journey]);
 
   const { lng, lat, zoom } = lngLatZoom;
   return (
