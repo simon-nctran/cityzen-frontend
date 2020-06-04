d277eb3b9202962b3dcc109609102b60d9107cce
Tarra Co <tarra.co@me.com>
2020-06-04 23:58:55 +1000

    update hide/show favourites

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,42 +1,60 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useUser } from "../api/apiUser";
+
+import { Button } from "react-bootstrap";
 
 import UserContext from "../UserContext";
 
 export default function Favourites() {
   const { token } = useContext(UserContext);
+  const [showFavourites, setShowFavourites] = useState(false);
 
   const { loading, userData, error } = useUser(token);
 
+  if (loading) {
+    return <p>Hold tight while we get your Favourites...</p>;
+  }
+  if (error) {
+    return <p>We weren't able to get your Favourites :'(: {error.message}</p>;
+  }
+
+  console.log(userData.searchOptions);
+
   return (
-    <div className="favourites-list">
-      {loading ? (
-        <h1>Loading list...</h1>
-      ) : (
-        <>
-          {error ? (
-            <h1>Error output, {error.message}</h1>
-          ) : (
-            <>
-              {/* <p>Intended output using {userData.username} </p> */}
-              <div>
-                These are your Saved Search Options:
-                {userData.searchOptions.map((search) => {
-                  return (
-                    <li key={search._id}>
-                      {/* key attribute shouldn't be too important */}
-                      {/* https://reactjs.org/docs/lists-and-keys.html */}
-                      {/* https://reactjs.org/docs/reconciliation.html#recursing-on-children */}
-                      Origin: {search.origin}, Destination: {search.destination}
-                      , POI: {search.poi}, Mode: {search.mode}
-                    </li>
-                  );
-                })}
-              </div>
-            </>
-          )}
-        </>
-      )}
+    <div className="favourites">
+      <Button
+        className={"btn"}
+        onClick={() => setShowFavourites(!showFavourites)}
+      >
+        {showFavourites ? "Hide Favourites" : "Show Favourites"}
+      </Button>
+      <FavouritesExtended userData={userData} showFavourites={showFavourites} />
+    </div>
+  );
+}
+
+function Favourite(favourite) {
+  const { id, origin, destination, poi, mode } = favourite;
+
+  return (
+    <div className={`favourite favourite-${id}`} key={id}>
+      <div className="favouriteDetails">
+        Origin: {origin} | Destination: {destination} | Point of Interest: {poi}{" "}
+        | Mode: {mode}
+      </div>
+    </div>
+  );
+}
+
+function FavouritesExtended(props) {
+  const { userData, showFavourites } = props;
+
+  return (
+    <div className={`favourites-shown ${showFavourites ? "show" : ""}`}>
+      <h3>Favourite Options</h3>
+      {userData.searchOptions.map((favourite) => (
+        <Favourite key={favourite.id} {...favourite} />
+      ))}
     </div>
   );
 }
