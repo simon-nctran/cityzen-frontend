a43af08e351652c3402f7ec54f82032b3b8f49c3
Tarra Co <tarra.co@me.com>
2020-06-05 15:56:42 +1000

    add team name and reformat

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+
+import { Container, Row, Col } from "react-bootstrap";
+
+import UserContext from "../UserContext";
 
 // App Navigation Component
 export default function Nav() {
+  const { userStatus } = useContext(UserContext);
+  const { userData, error } = userStatus;
+
   return (
     <nav>
-      <NavLink exact to="/">
-        Home
-      </NavLink>
-      <NavLink to="/profile">Profile</NavLink>
+      <Container className="navigation">
+        <Row>
+          <Col xs="auto" className="title">
+            <h2>CITYZEN</h2>
+          </Col>
+          <Col></Col>
+          <Col xs="auto">
+            <NavLink exact to="/">
+              <p>Home</p>
+            </NavLink>
+            <NavLink to="/profile">
+              {userData !== null && error == null ? (
+                <p>Hello {userData.username}!</p>
+              ) : (
+                "Profile"
+              )}
+            </NavLink>
+          </Col>
+        </Row>
+      </Container>
     </nav>
   );
 }
