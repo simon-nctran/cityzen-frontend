22a4045fa2997a45c3f86e8e4d9d9383ab1b19d1
Tarra Co <tarra.co@me.com>
2020-06-06 23:30:22 +1000

    update Navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,27 +11,34 @@ export default function Nav() {
   const { userData, error } = userStatus;
 
   return (
-    <nav>
-      <Container className="navigation">
+    <div className="navigation">
+      <Container>
         <Row>
           <Col xs="auto" className="title">
-            <h2>CITYZEN</h2>
+            <NavLink to="/journey">
+              <h2>ON THE WAY</h2>
+            </NavLink>
           </Col>
           <Col></Col>
-          <Col xs="auto">
-            <NavLink exact to="/">
-              <p>Home</p>
-            </NavLink>
-            <NavLink to="/profile">
-              {userData !== null && error == null ? (
-                <p>Hello {userData.username}!</p>
-              ) : (
-                "Profile"
-              )}
-            </NavLink>
-          </Col>
+          <nav>
+            <Col xs="auto">
+              <NavLink exact to="/">
+                <p>Home</p>
+              </NavLink>
+              <NavLink to="journey">
+                <p>Journey</p>
+              </NavLink>
+              <NavLink to="/profile">
+                {userData !== null && error == null ? (
+                  <p>Hello {userData.username}!</p>
+                ) : (
+                  <p>Profile</p>
+                )}
+              </NavLink>
+            </Col>
+          </nav>
         </Row>
       </Container>
-    </nav>
+    </div>
   );
 }
