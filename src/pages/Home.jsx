9e288e1adb85f37272964fb3f54b491c4cb8a94e
Tarra Co <tarra.co@me.com>
2020-06-07 15:05:33 +1000

    update page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,82 @@
-import React from "react";
-import { Container, Button } from "react-bootstrap";
+import React, { useState, useContext } from "react";
+
+import Header from "../components/Header";
+import Input from "../components/Input";
+import Favourites from "../components/Favourites";
+import Map from "../components/Map";
+
+import UserContext from "../UserContext";
 
 export default function Home() {
+  const { userStatus } = useContext(UserContext);
+  const { userData, error } = userStatus;
+  const [initial, setInitial] = useState(true);
+
+  const [journey, setJourney] = useState({});
+
+  function getWayPoints(points) {
+    setJourney(points);
+    console.log("The Journey is:", points);
+  }
+
   return (
-    <div className="home" align="middle">
-      <Container>
-        <h1>WELCOME TO ON THE WAY!</h1>
-        <br />
-        <h4>
-          This app will help you find services and points of interest from your
-          origin on the way to your destination!
-        </h4>
-        <br />
-        <Button href="/journey"> Get Started!</Button>
-        <br />
-        <br />
-        <p>This app was created by the members of Cityzen.</p>
-        <p>The University of Melbourne (2020)</p>
-      </Container>
+    <div className="main">
+      <div className="header">
+        <Header />
+      </div>
+
+      <div className="input">
+        <Input getWayPoints={getWayPoints} setInitial={setInitial} />
+      </div>
+
+      <div className="favourites">
+        {userData !== null && error == null ? (
+          <Favourites getWayPoints={getWayPoints} setInitial={setInitial} />
+        ) : (
+          <> </>
+        )}
+      </div>
+      {initial ? (
+        <div className="home" align="middle">
+          <div className="welcome">
+            {console.log(initial)}
+            <h1>
+              <span role="img" aria-label="cityscape">
+                🏙
+              </span>{" "}
+              Welcome to Cityzen!{" "}
+              <span role="img" aria-label="cityscape">
+                🏙
+              </span>
+            </h1>
+            <h3>
+              This app will help you find services and points of interest from
+              where you are to where you want to go!
+            </h3>
+            <br />
+            <h4>
+              <span role="img" aria-label="arrow-up">
+                ⬆️
+              </span>{" "}
+              Fill out the above form to get started!{" "}
+              <span role="img" aria-label="arrow-up">
+                ⬆️
+              </span>
+            </h4>
+          </div>
+          <div>
+            <footer>
+              <p>This app was created by the members of Cityzen.</p>
+              <p>The University of Melbourne, 2020</p>
+            </footer>
+          </div>
+        </div>
+      ) : (
+        <div className="map">
+          {console.log(initial)}
+          <Map journey={journey} />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container, Button } from "react-bootstrap";
+
+export default function Landing() {
+  return (
+    <div className="home" align="middle">
+      <Container>
+        <h1>WELCOME TO ON THE WAY!</h1>
+        <br />
+        <h4>
+          This app will help you find services and points of interest from your
+          origin on the way to your destination!
+        </h4>
+        <br />
+        <Button href="/journey"> Get Started!</Button>
+        <br />
+        <br />
+        <p>This app was created by the members of Cityzen.</p>
+        <p>The University of Melbourne (2020)</p>
+      </Container>
+    </div>
+  );
+}
